56c50574a556c3cb34609831fbdbc2bc71b7e352
giliover <giliover@hotmail.com>
2020-12-02 19:14:31 -0300

    Add: configuration global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+  body {
+    backgroud: #312R38;
+    color: #FFF;
+    --webkit-font-smoothinh: antialiased;
+  }
+  body {
+    background: #312E38;
+  }
+`;
